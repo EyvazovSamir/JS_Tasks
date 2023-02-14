310beeda1962ebc2a7b615cb6d22a10bb2734362
Samirka7 <90614000+Samirka7@users.noreply.github.com>
2023-02-14 15:13:40 +0700

    add task1

---
+++ app.js
@@ -0,0 +1,14 @@
+let width = prompt("Enter width:");
+let height = prompt("Enter height:");
+let area = Number(width) * Number(height);
+alert(area);
+
+let birthDate = prompt("Enter your birth date:");
+let currentYear = prompt("Enter current year:");
+let age = Number(currentYear) - Number(birthDate);
+alert(`${age} years`);
+
+let km = prompt("Enter km:");
+const mil = 0.6214;
+let userDeyeri = Number(mil) * Number(km);
+alert(`${userDeyeri} mil`);
