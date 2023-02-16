85b92577b88ebda03306d0ab75de99edd22e76b0
Samirka7 <90614000+Samirka7@users.noreply.github.com>
2023-02-16 13:24:56 +0700

    add task2

---
+++ task2/app.js
@@ -0,0 +1,12 @@
+let ad = prompt("Enter ad:");
+let soyad = prompt("Enter soyad:");
+let adSoyad = ad + " " + soyad;
+console.log(adSoyad);
+
+let username = prompt("Enter username:");
+console.log("~ ~ " + username + " ~ ~");
+
+let width = prompt("Enter width = ");
+let length = prompt("Enter length = ");
+let cemi = (Number(width) + Number(length)) * 2;
+console.log(cemi);
