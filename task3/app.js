0c6f0127f94844e13306263c239c4a7c494f74d2
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-02-22 11:33:53 +0400

    bug report 🐛(task 5, 2nd part)

--- task3/app.js
+++ task3/app.js
@@ -1,14 +1,17 @@
 // task 1 sqrt
+// A: done
 let count = Number(prompt("Enter count: "));
 let squareRoot = Math.sqrt(count); //
 console.log(squareRoot);
 
 // task2 fahrenheit
+// A: done
 let celsius = Number(prompt("Enter celsius: "));
 let fahrenheit = celsius * 1.8 + 32;
 console.log(fahrenheit);
 
 // task3 > < = 0
+// // A: done
 let number = Number(prompt("Enter number: "));
 if (number > 0) {
   console.log("number is positive");
@@ -21,7 +24,7 @@ if (number > 0) {
 }
 
 // task4 even or odd
-
+// A: done
 let num = Number(prompt("Enter number: "));
 if (num % 2 == 0) {
   console.log("number is even");
@@ -30,7 +33,7 @@ if (num % 2 == 0) {
 }
 
 // task5 max number
-
+// A: done
 let eded1 = Number(prompt("Enter first number: "));
 let eded2 = Number(prompt("Enter second number: "));
 let eded3 = Number(prompt("Enter third number: "));
@@ -39,7 +42,7 @@ console.log(Math.max(eded1, eded2, eded3));
 let number1 = Number(prompt("Enter first number: "));
 let number2 = Number(prompt("Enter second number: "));
 let number3 = Number(prompt("Enter third number: "));
-
+// A: else-e yazma. cunki, diger hallarda da ola biler, else-e daxil olsun. 
 if (number1 >= number2 && number1 >= number3) {
   console.log(number1);
 } else if (number2 >= number1 && number2 >= number3) {
@@ -49,7 +52,7 @@ if (number1 >= number2 && number1 >= number3) {
 }
 
 // task6
-
+// A: done
 let n1 = Number(prompt("Enter first number: "));
 let n2 = Number(prompt("Enter second number: "));
 let operator = prompt("Enter operator: ");
@@ -61,7 +64,7 @@ if (operator === "+") {
 } else {
   console.log("error");
 }
-
+// A: done
 let nb1 = Number(prompt("Enter first number: "));
 let nb2 = Number(prompt("Enter second number: "));
 let operator1 = prompt("Enter operator: ");
