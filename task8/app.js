7450509a2d12a4ff674673c43ab7b3a550cc905d
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-03-06 12:28:45 +0400

    live legend 🐱‍💻

--- task8/app.js
+++ task8/app.js
@@ -1,5 +1,5 @@
 // 1
-
+// A: ela
 function prop2Taker(obj) {
   console.log(obj["prop-2"]);
 }
@@ -8,7 +8,7 @@ prop2Taker({ one: 1, "prop-2": 2 }); // 2
 prop2Taker({ "prop-2": "two", prop: "test" }); // 'two'
 
 // 2
-
+// A: ela
 function propertyTaker(obj1, propertyName) {
   console.log(obj1[propertyName]);
 }
@@ -18,7 +18,7 @@ propertyTaker({ country: "Sweden", continent: "Europe" }, "country"); // 'Sweden
 propertyTaker({ name: "ali", age: 12 }, "name"); // 'ali'
 
 // 3
-
+// A: ela
 function existsAndTruthy(obj2, propertyName2) {
   if (obj2[propertyName2]) {
     console.log(true);
